811b6aef90b8fe7dce25bca63a10fa3fb2f017bf
edugmenes <edugmenes1@hotmail.com>
2024-08-07 18:55:37 -0300

    first commit

---
+++ exercicios/exercicios.js
@@ -0,0 +1,29 @@
+// //1)
+// function cumprimentar(nome){
+//     return `Olá, ${nome}!`
+// }
+
+// console.log(cumprimentar("Leonardo"))
+// console.log(cumprimentar("Maria"))
+
+
+// //2)
+// function converterIdadeEmAnosParaDias(idadeEmAnos){
+//     const diasDoAno = 365
+//     return idadeEmAnos * diasDoAno
+// }
+
+// console.log(converterIdadeEmAnosParaDias(25))
+// console.log(converterIdadeEmAnosParaDias(70))
+
+
+//3)
+// function calcularSalario(horasTrabalhadas, ganhoPorHora){
+//     var salario = horasTrabalhadas * ganhoPorHora;
+//     return `Salário igual a R$ ${salario}`
+// }
+
+// console.log(calcularSalario(150, 40.5))
+
+
+//4)
---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,17 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2* a
+}
+
+dobro = (a) => 2 * a
+console.log(dobro(3))
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+console.log(ola())
